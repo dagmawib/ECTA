ba5b0ef3843f8e4eeb974c592e704d30264ab59b
dagmawib <dagmawibelayneh14@gmail.com>
2026-01-22 17:25:59 +0300

    feat(client): initialize Next.js client with Tailwind CSS and basic layout
    - Added package.json for client dependencies and scripts
    - Created postcss.config.mjs for Tailwind CSS integration
    - Added globals.css to import Tailwind CSS styles
    - Implemented layout.js for the root layout with Google Fonts
    - Created page.js for the home page with a simple greeting

---
+++ client/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans">
+      <h1 className="text-4xl text-black-800">Hello, World!</h1>
+    </div>
+  );
+}
